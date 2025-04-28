a045ba4837985b36761f0e7e90aac6cd117787ef
Marko0D <dusic.m55@gmail.com>
2025-04-28 19:32:10 +0200

    Update 3..py

--- 23.4.2025/3..py
+++ 23.4.2025/3..py
@@ -7,3 +7,15 @@ def je_prost(broj):
         return True
 print(je_prost(7))
 print(je_prost(8))
+#ili ovako:
+def je_prost(broj):
+    brojac=0
+    for i in range(broj,1,-1):
+        if broj%i==0:
+           brojac=brojac+1
+    if brojac>2:
+        return False
+    else:
+        return True
+x=int(input("unesi broj"))
+print(je_prost(x))
