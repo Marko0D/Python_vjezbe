328034294640fb36ab227c93f6c85d1f5466b8d8
Marko0D <dusic.m55@gmail.com>
2025-05-04 01:29:00 +0200

    Delete 16.4.2025 directory

--- 16.4.2025/3..py
+++
@@ -1,7 +0,0 @@
-def je_palindrom(rijec):
-    if rijec[::-1]==rijec:
-        return True
-    else:
-        return False
-print(je_palindrom("radar")) # Treba ispisati: True
-print(je_palindrom("Python")) # Treba ispisati: False
--- 16.4.2025/BONUS.py
+++
@@ -1,7 +0,0 @@
-def broji_samoglasnike(string):
-    broj_samoglasnika=0
-    for i in string.lower():
-        if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
-            broj_samoglasnika=broj_samoglasnika+1
-    return broj_samoglasnika
-print(broji_samoglasnike("Python Programiranje")) # Treba ispisati: 6
