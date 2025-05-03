44b52bacce148a206792be5bc2eaa32dc17e2b80
Marko0D <dusic.m55@gmail.com>
2025-05-04 01:27:34 +0200

    Delete 2.4.2025 directory

--- 2.4.2025/1..py
+++
@@ -1,9 +0,0 @@
-def zbroj(x,y):
-    return x+y
-a=int(input("Unesi prvi broj:"))
-b=int(input("Unesu drugi broj:"))
-x=zbroj(a,b)
-c=int(input("Unesi treći broj:"))
-d=int(input("Unesi četvrti broj:"))
-y=zbroj(c,d)
-print("Zbroj unesenih brojeva je", zbroj(x,y))
--- 2.4.2025/2..py
+++
@@ -1,9 +0,0 @@
-def opseg(x,y):
-    o=2*x+2*y
-    return o
-def povrsina(x,y):
-    p=x*y
-    return p
-a=int(input("Unesi duljinu stranice a pravokutnika:"))
-b=int(input("Unesi duljinu stranice b pravokutnika:"))
-print("Opseg pravokutnika je", opseg(a,b), ", a površina je", povrsina(a,b))
